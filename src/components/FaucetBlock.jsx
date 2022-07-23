256890c3ad87833f1fe4677cdc1cb15b064bed7b
Agustin Kassis <webmaster@masize.com>
2022-07-23 17:52:19 -0300

    removed usdc token

--- src/components/FaucetBlock.jsx
+++ src/components/FaucetBlock.jsx
@@ -25,12 +25,12 @@ export const FaucetBlock = ({ address }) => {
             image={peImage}
           />
           <br />
-          <AddToken
+          {/* <AddToken
             address={"0x2791bca1f2de4661ed88a30c99a7a9449aa84174"}
             symbol={"USDC"}
             decimals={6}
             image={usdcImage}
-          />
+          /> */}
           <br />
           <Button
             type='button'
